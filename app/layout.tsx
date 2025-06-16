64cc4d282427f205ac6be389695dee63d5f2ba15
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2025-06-16 22:24:52 +0500

    rabsha done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Cross Protection & Logistics',
+  icons: {
+    icon: '/images/favicon.png', 
+  },
 };
 
 export default function RootLayout({
