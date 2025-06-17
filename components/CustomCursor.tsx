ccd25d1f464e9c85e538af9b9e3785e2b8d0ff56
Abdul Basit Shakeel <basitshakeel2005@gmail.com>
2025-06-17 21:33:39 +0500

    done

--- components/CustomCursor.tsx
+++ components/CustomCursor.tsx
@@ -33,7 +33,7 @@ const CustomCursor = () => {
   return (
     <motion.div
       className="fixed top-0 left-0 z-[9999] pointer-events-none"
-      animate={{ x: position.x - 12, y: position.y - 12, opacity: isVisible ? 1 : 0 }}
+      animate={{ x: position.x - 10, y: position.y - 12, opacity: isVisible ? 1 : 0 }}
       transition={{ type: "spring", stiffness: 500, damping: 30 }}
     >
       <img src="/images/cursor2.png" className="w-8 h-8" />
